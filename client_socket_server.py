d5bc5a7f1276f724903e3001e936f7fd67c1c90f
VanishingPoint <130802194+VanishingPoint@users.noreply.github.com>
2025-01-31 13:45:47 -0800

    Local test of socket server

--- client_socket_server.py
+++
@@ -1,9 +0,0 @@
-import socket               
-
-s = socket.socket()        
-host = '100.120.18.53'# ip of raspberry pi
-#TODO: Match the ip in the server script once changed
-port = 8085               
-s.connect((host, port))
-print(s.recv(1024))
-s.close()
