33ffde170e2a986b640aff0f010fc695d48c70c8
VanishingPoint <130802194+VanishingPoint@users.noreply.github.com>
2025-02-01 16:30:23 -0800

    Basic template for eye tracking video created, with comments indicating where more needs to be written/determined.

---
+++ eyetrack_pi.py
@@ -0,0 +1,32 @@
+import time
+from picamera2 import Picamera2
+
+#TODO:Display instructions, display calibration, display two dots, trigger this from client
+#Feed videos into interpreter, get gaze data, score, send score
+
+# Initialize the cameras
+left_camera = Picamera2(0)
+right_camera = Picamera2(1)
+
+# Configure the cameras
+left_camera.configure(left_camera.create_video_configuration())
+right_camera.configure(right_camera.create_video_configuration())
+
+#TODO: Figure out the right config values for the quality and framerate that is best
+
+# Start recording
+left_camera.start_recording("Left.h264")
+right_camera.start_recording("Right.h264")
+
+# Record for 120 seconds
+time.sleep(120)
+
+# Stop recording
+left_camera.stop_recording()
+right_camera.stop_recording()
+
+# Close the cameras
+left_camera.close()
+right_camera.close()
+
+#NOTE: If we cannot process the videos on the pi, we can "stream" the videos using a socket.
