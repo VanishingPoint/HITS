6ea5cfacf2f283c623e9d8ec83270673ef94d228
ChanelR <chanelrobertson7@gmail.com>
2025-02-03 14:50:43 -0800

    Added UI for eye tracking

--- pdf_to_png.py
+++ pdf_to_png.py
@@ -4,13 +4,13 @@ from pdf2image import convert_from_path
 # Cognitive Proctor Images = cognitive_page_#.png
 # Cognitive Participant Images = page_#.png
 # Balance Proctor Images = balance_page_#.png
-# Eye Tracking Proctor Images = eyetrackingproctor_page_#.png
+# Eye Tracking Proctor Images = eyetrackingproctor_#.png
 # Eye Tracking Participant Images = eyetracking_page#.png
 # Main Menu Proctor Images = menu_#.png
 
 # Specify the path to your PDF and Poppler's bin folder
 
-pdf_path = r"C:\Users\chane\Downloads\Main Menu Proctor Images.pdf"
+pdf_path = r"C:\Users\chane\Downloads\Eye Tracking Participant Images.pdf"
 
 poppler_path = r"C:\Users\chane\Downloads\Release-24.08.0-0\poppler-24.08.0\Library\bin"
 
@@ -19,8 +19,8 @@ images = convert_from_path(pdf_path, dpi=500, poppler_path=poppler_path)
 
 # Save each page as a separate PNG file
 
-output_folder = r'C:\Users\chane\Desktop\HITS\HITS\Main Menu Proctor Images'
+output_folder = r'C:\Users\chane\Desktop\HITS\HITS\Eye Tracking Participant Images'
 for i, image in enumerate(images, start=0):
-    image.save(f'{output_folder}/menu_{i}.png', 'PNG')
+    image.save(f'{output_folder}/eyetracking_{i}.png', 'PNG')
 
 print("Conversion complete!")
