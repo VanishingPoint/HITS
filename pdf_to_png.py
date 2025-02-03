7d407dd96ff8f6515db883e7de822dc711558117
ChanelR <chanelrobertson7@gmail.com>
2025-02-03 14:32:33 -0800

    updated main menu images

--- pdf_to_png.py
+++ pdf_to_png.py
@@ -6,11 +6,12 @@ from pdf2image import convert_from_path
 # Balance Proctor Images = balance_page_#.png
 # Eye Tracking Proctor Images = eyetrackingproctor_page_#.png
 # Eye Tracking Participant Images = eyetracking_page#.png
+# Main Menu Proctor Images = menu_#.png
 # Main menu Images TO DO
 
 # Specify the path to your PDF and Poppler's bin folder
 
-pdf_path = r"C:\Users\chane\Downloads\Cognitive Proctor Images.pdf"
+pdf_path = r"C:\Users\chane\Downloads\Main Menu Proctor Images.pdf"
 
 poppler_path = r"C:\Users\chane\Downloads\Release-24.08.0-0\poppler-24.08.0\Library\bin"
 
@@ -19,8 +20,8 @@ images = convert_from_path(pdf_path, dpi=500, poppler_path=poppler_path)
 
 # Save each page as a separate PNG file
 
-output_folder = r'C:\Users\chane\Desktop\HITS\HITS\Cognitive Proctor Images'
+output_folder = r'C:\Users\chane\Desktop\HITS\HITS\Main Menu Proctor Images'
 for i, image in enumerate(images, start=0):
-    image.save(f'{output_folder}/cognitive_page_{i}.png', 'PNG')
+    image.save(f'{output_folder}/menu_{i}.png', 'PNG')
 
 print("Conversion complete!")
