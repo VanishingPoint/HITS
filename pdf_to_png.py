2149fc9391702e9bb3181ea0796df557855e78d2
ChanelR <chanelrobertson7@gmail.com>
2025-02-03 18:49:00 -0800

    updated balance procedure

--- pdf_to_png.py
+++ pdf_to_png.py
@@ -10,7 +10,7 @@ from pdf2image import convert_from_path
 
 # Specify the path to your PDF and Poppler's bin folder
 
-pdf_path = r"C:\Users\chane\Downloads\Eye Tracking Participant Images.pdf"
+pdf_path = r"C:\Users\chane\Downloads\Balance Proctor Images.pdf"
 
 poppler_path = r"C:\Users\chane\Downloads\Release-24.08.0-0\poppler-24.08.0\Library\bin"
 
@@ -19,8 +19,8 @@ images = convert_from_path(pdf_path, dpi=500, poppler_path=poppler_path)
 
 # Save each page as a separate PNG file
 
-output_folder = r'C:\Users\chane\Desktop\HITS\HITS\Eye Tracking Participant Images'
+output_folder = r'C:\Users\chane\Desktop\HITS\HITS\Balance Proctor Images'
 for i, image in enumerate(images, start=0):
-    image.save(f'{output_folder}/eyetracking_{i}.png', 'PNG')
+    image.save(f'{output_folder}/balance_page_{i}.png', 'PNG')
 
 print("Conversion complete!")
