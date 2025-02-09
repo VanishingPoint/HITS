407c3413e811a9e3fd3f1a05bbcdfca47257e8dd
VanishingPoint <130802194+VanishingPoint@users.noreply.github.com>
2025-02-08 18:07:55 -0800

    Updated file paths

--- Cognitive/cognitive_pi.py
+++ Cognitive/cognitive_pi.py
@@ -24,7 +24,7 @@ image_word = {
 
 # List of image paths based on shuffled numbers
 image_paths = [
-    fr"/home/hits/Documents/GitHub/HITS/Cognitive Participant Images/page_{num}.png"
+    fr"/home/hits/Documents/GitHub/HITS/Cognitive/Cognitive Participant Images/page_{num}.png"
     for num in image_numbers
 ]
 
